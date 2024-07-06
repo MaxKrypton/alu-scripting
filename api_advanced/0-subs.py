8295282b745b8e239cdb4f46dbe2b0f45c66ada9
Gentil Quentin <q.rurangirw@alustudent.com>
2024-07-06 21:32:40 +0200

    Commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""get subscribers numbers function"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
-
-import json
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """get all subscribers"""
-    if len(sys.argv) < 2:
-        return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
-    
+        return 0 
