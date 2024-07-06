1305cf1742d2fc7421b04029dcb42b1a5cd51231
Gentil Quentin <q.rurangirw@alustudent.com>
2024-07-06 21:10:25 +0200

    Commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-"""get subscribers numbers function"""
-
-
-import json
+"""
+Returns the number of subscribers from a subreddit
+"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """get all subscribers"""
-    if len(sys.argv) < 2:
+    """ Set a custom header user-agent """
+    headers = {"User-Agent": "ALU-scripting API 0.1"}
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+
+    try:
+        response = requests.get(url, headers=headers,
+                                timeout=30, allow_redirects=False)
+
+    except requests.exceptions.Timeout:
+        return "The request Timed out"
+
+    if response.status_code == 200:
+        json_data = response.json()
+        subscriber_number = (
+            json_data.get("data")
+            .get("children")[0]
+            .get("data")
+            .get("subreddit_subscribers")
+        )
+        return subscriber_number
+    elif response.status_code == 404:
         return 0
     else:
-        url = "https://api.reddit.com/r/{}/about".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
+        return 0
